c937c3319d3d9caa8b472cc04b3b83647937a216
NateDhaliwal <nathaniel.shaan@gmail.com>
2026-07-24 15:26:07 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: see-tl3-progress
+# about: See your own progress to TL3.
 # meta_topic_id: TODO
 # version: 0.0.1
-# authors: Discourse
+# authors: Nate Dhaliwal
 # url: TODO
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :see_tl3_progress_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+module ::SeeTl3Progress
+  PLUGIN_NAME = "see-tl3-progress"
 end
 
-require_relative "lib/my_plugin_module/engine"
+require_relative "lib/see_tl3_progress/engine"
 
 after_initialize do
   # Code which should run after Rails has finished booting
